6fc6a28b34cb96fa02d8991c321f24b59f348609
aditiii-s <mycollegeworkk@gmail.com>
2026-06-25 03:27:56 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,21 @@
+//here we learn about different data types 
+"use strict";//treat all js code with newer version 
+//alert(3+3)--- we are using nodejs ,not browser
+console.log(3+
+    3)//code readibility should be recognizable 
+    console.log("aditi")
+    /*
+    types of data types 
+    numbr is greater than 2 power 53 we use bigint
+    string= ""
+    boolean = t/f
+    null= it mean there is any char but its empty
+    undefined = not assigned 
+    symbol = unique 
+    THESES ARE PRIMITIVE DATA TYPES WE USED  
+    
+    */
+   //OBJECT
+   console.log(typeof undefined);//iska type undefined hi hota hai
+   console.log(typeof null);//iska type mostly object deta hai output mei 
+   console.log(typeof int);//gives number 
