78436598d79dca1a2542da059677bd9359a18260
Aditi <mycollegeworkk@gmail.com>
2026-08-07 17:30:16 +0000

    learning about objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -21,3 +21,13 @@ const regularuser={
     }
 }
 //console.log(regularuser.fullname.userfullname);
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",2:"b"}//3-->key,a-->value 
+const obj4={5:"a",6:"b"}
+
+//const obj3={obj1,obj2}-->object k andar obj print kr dega 
+//const obj3=object.assign({},obj1,obj2,obj4)
+
+const obj3={...obj1,...obj2}//--> "..."-->spread feature k use hua hai 
+console.log(obj3);
