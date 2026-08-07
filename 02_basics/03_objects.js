a78c0d1a373ef6f6316f5a468e1e95ec73ed1552
Aditi <mycollegeworkk@gmail.com>
2026-08-07 17:29:36 +0000

    objects 2 topic

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,23 @@
+//singleton js--- 
+//const tinderUser=new Object()
+
+//NON SINGLETON 
+/* ----    const tinderUser={}
+tinderUser.id="123abc"
+tinderUser.name="samay raina "
+tinderUser.isLoggedIn=false
+
+console.log(tinderUser);   --------------*/
+const regularuser={
+    email:"samay@gmail.com",
+    fullname:{
+        userfullname:
+        {
+            firstname:"aadvik",
+            lastname:"singh"
+        }
+
+
+    }
+}
+//console.log(regularuser.fullname.userfullname);
